aa8c6bac8b8a543183e6a8f74707e2fef8869778
vsi-fi <vsi@kapsi.fi>
2024-02-16 14:21:15 +0100

    dwm start

---
+++ dwm/dwm-start.sh
@@ -0,0 +1,13 @@
+while true; do
+battery=`/usr/bin/acpi | awk '{print $3 $4 $5 " " $6 " " $7}'`;
+memfreak2=`grep MemFree /proc/meminfo | awk '{ print $2 }'`;
+memfreak=$(( $memfreak2/1024 ));
+ssid=`/usr/bin/nmcli -t -f active,ssid dev wifi|grep -oPi '(?:yes:)\K(.*?)$'`;
+CLK=$( date +'%a %b %d %R:%S %Z' )
+AVG=$( cat /proc/loadavg | cut -d ' ' -f -3 )
+xsetroot -name "| Load $AVG | Mem: $memfreak MB free | $battery | SSID: $ssid | $CLK "
+sleep 5 
+done &
+
+exec /usr/bin/dwm > /dev/null
+
