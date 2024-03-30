92dbe4dd044dbcdc6b6c15848bf593301afc1fe5
vsi <vsi@kapsi.fi>
2024-03-30 06:59:15 +0200

    iw/nmcl + dwm-6.5

--- dwm/dwm-start.sh
+++ dwm/dwm-start.sh
@@ -2,7 +2,8 @@ while true; do
 battery=`/usr/bin/acpi | awk '{print $3 $4 $5 " " $6 " " $7}'`;
 memfreak2=`grep MemFree /proc/meminfo | awk '{ print $2 }'`;
 memfreak=$(( $memfreak2/1024 ));
-ssid=`/usr/bin/nmcli -t -f active,ssid dev wifi|grep -oPi '(?:yes:)\K(.*?)$'`;
+#ssid=`/usr/bin/nmcli -t -f active,ssid dev wifi|grep -oPi '(?:yes:)\K(.*?)$'`;
+ssid=`/usr/bin/iw dev wlp2s0 info|grep -oPi 'ssid \K(.*?)$'`;
 CLK=$( date +'%a %b %d %R:%S %Z' )
 AVG=$( cat /proc/loadavg | cut -d ' ' -f -3 )
 xsetroot -name "| Load $AVG | Mem: $memfreak MB free | $battery | SSID: $ssid | $CLK "
